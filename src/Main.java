9c442d28e477032bef26f977e27d5153e0d5df17
RenatTagitov <rena.tagirov@gmail.com>
2021-02-26 23:32:57 +0300

    Добавил описание

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+
+        //Простой калькулятор работающий не только с простыми числами, но и с римскими
+        //Варианты операций: + - * /
+        //Можно вводить числа от 1 до 100
+        //Можно вводить римские числа от I до C
+        //Памятка:*
+        //Десятичные: I II III IV V VI VII VIII IX X
+        //Сотые:  X XX XXX XXXX L LX LXX LXXX LXXXX C
+        //Тысячные: C CC CCC CD D DC DCC DCCC CM M
+        //Десятитысячные: M MM MMM MV V VM VMM VMMM MX X
+
         SumOfNum sumNumber = new SumOfNum();
         sumNumber.sum();
     }
